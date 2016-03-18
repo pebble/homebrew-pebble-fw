75af85717ce908b6f45cc5e1aa475d7b7a64c1b5
astapleton <astapleton@pebble.com>
2016-03-18 16:44:40 -0700

    Update arm-none-eabi-gcc.rb

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -1,6 +1,6 @@
 class ArmNoneEabiGcc < Formula
   homepage "https://launchpad.net/gcc-arm-embedded"
-  version "20140805"
+  version "20150921"
   url "https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/gcc-arm-none-eabi-4_9-2015q3-20150921-mac.tar.bz2"
   sha256 "a6353db31face60c2091c2c84c902fc4d566decd1aa04884cd822c383d13c9fa"
 
