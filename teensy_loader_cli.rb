2fff26b3bbfb2b69c54c0432cb6570d977be293c
bgomberg <bgomberg@getpebble.com>
2016-07-01 12:12:18 -0700

    Fixed homepage

--- teensy_loader_cli.rb
+++ teensy_loader_cli.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class TeensyLoaderCli < Formula
-  homepage "https://github.com/pebble/qemu"
+  homepage "https://github.com/pebble/teensy_loader_cli"
   url "git@github.com:pebble/teensy_loader_cli.git", :branch => "mac_libusb", :using => :git
 
   def install
