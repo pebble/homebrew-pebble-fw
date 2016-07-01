165c7b87ed25066e2bf45ae8f551ab6ed5563146
bgomberg <bgomberg@getpebble.com>
2016-07-01 12:14:49 -0700

    Added version

--- teensy_loader_cli.rb
+++ teensy_loader_cli.rb
@@ -4,6 +4,8 @@ class TeensyLoaderCli < Formula
   homepage "https://github.com/pebble/teensy_loader_cli"
   url "git@github.com:pebble/teensy_loader_cli.git", :branch => "mac_libusb", :using => :git
 
+  version "1.0"
+
   def install
     system 'make'
     bin.install 'teensy_loader_cli'
